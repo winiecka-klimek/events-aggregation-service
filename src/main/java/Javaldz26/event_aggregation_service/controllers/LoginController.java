f0a1e275c892a32c404a428b7954547aeaea0e99
Winiecka-Klimek <marta.winiecka@gmail.com>
2021-01-11 22:18:06 +0100

    navbar update

--- src/main/java/Javaldz26/event_aggregation_service/controllers/LoginController.java
+++ src/main/java/Javaldz26/event_aggregation_service/controllers/LoginController.java
@@ -1,6 +1,8 @@
 package Javaldz26.event_aggregation_service.controllers;
 
+import Javaldz26.event_aggregation_service.services.LoginService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,6 +11,13 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Slf4j
 @Controller
 public class LoginController {
+
+    public LoginController(LoginService loginService) {
+        this.loginService = loginService;
+    }
+
+    @Autowired
+    private final LoginService loginService;
 
     @GetMapping("/login")
     public String showLoginForm(){
